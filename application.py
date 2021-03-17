6d68960d0c449387c32a69df2876be61c4e0c113
juan camilo caviedes <jcamilocc97@gmail.com>
2021-03-16 23:20:24 -0500

    add bulma css

--- application.py
+++ application.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/bank-marketing/')
+@app.route('/bank-marketing/', methods=["GET", "POST"])
 def bank_marketing():
     return render_template('bankMarketing.html', name="holanda")
